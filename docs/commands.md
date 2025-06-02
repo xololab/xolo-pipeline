5710bdc3854516e19bc66bac75208f7b1c4c2978
Ronny Ascencio <ronnycompartist@gmail.com>
2025-06-02 19:19:36 -0400

    auto docs

---
+++ docs/commands.md
@@ -0,0 +1,7 @@
+# Core Module
+
+::: xolo.core.projects
+
+# Core Module
+
+::: xolo.core.models
--- xolo/core/models.py
+++ xolo/core/models.py
@@ -3,11 +3,17 @@
 from pydantic import BaseModel
 
 class ProjectData(BaseModel):
+    """
+    Metadata for the project config file
+    """
     name: str
     resolution: str
     fps: str
 
 class FolderStructure(BaseModel):
+    """
+    folder structure that will be created in the base path of the PROJECTS_ROOT
+    """
     assets: str = "/assets"
     shots: str = "/shots"
     renders: str = "/renders"
