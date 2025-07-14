026445638d067158bea1c85d9a17cef72f4ea7e5
Ronny Ascencio <ronnycompartist@gmail.com>
2025-07-13 22:01:16 -0400

    basic set up for shot folder naming convention

--- cli/commands/shot.py
+++ cli/commands/shot.py
@@ -11,7 +11,15 @@ def create(
         "", prompt="sequence name (eg. seq01)", show_default=False
     ),
     shot_id: str = typer.Option(
-        "", prompt="shot id (e.g. 0010, or 0020)", show_default=False
-    ),
-):
-    console.print(f"[green]✔ Shot Created : {sequence_name}_{shot_id}[/green]")
+        "", prompt="shot id (e.g. 0010, or 0020)", show_default=False)):
+
+
+    if sequence_name:
+
+        temp_name = [sequence_name, shot_id]
+
+        shotname = "_".join(temp_name)
+        console.print(f"[green]✔ Shot Created : {shotname}[/green]")
+    else:
+        shotname = shot_id
+        console.print(f"[green]✔ Shot Created : {shotname}[/green]")
